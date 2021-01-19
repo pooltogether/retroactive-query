0e460a474e16773b935274e809dd3230a7d7a36b
Aodhgan <aodhgan@pooltogether.com>
2021-01-19 13:35:50 -0800

    scaling factors

--- src/v2_usdc_pod.sql
+++ src/v2_usdc_pod.sql
@@ -70,24 +70,24 @@ CREATE TEMP FUNCTION
      create temp table pod_normalised_transfers AS (
      SELECT 
       address,
-      CAST(value AS numeric) * 1e12 / CAST(mantissa AS numeric) as value, -- normalizing to 18 decimals
+      CAST(value AS numeric)*1e6 / CAST(mantissa AS numeric) as value, -- normalizing to 18 decimals
       transaction_hash,
       block_number,
       log_index
       FROM( 
         SELECT *,      
         CASE
-            WHEN block_number < 10027387 THEN "999321063644775776713679" 
-            WHEN block_number BETWEEN 10027387 AND 10040222 THEN "998982525245810425296122"
-            WHEN block_number BETWEEN 10040222 AND 10065995 THEN "998707946932873941046141"
-            WHEN block_number BETWEEN 10065995 AND 10117398 THEN "998219452943352911059711"
-            WHEN block_number BETWEEN 10117398 AND 10169024 THEN "997414844714885566932754"
-            WHEN block_number BETWEEN 10169024 AND 10362451 THEN "997317071340081993793812"
-            WHEN block_number BETWEEN 10362451 AND 10375289 THEN "997075459772288395141677"
-            WHEN block_number BETWEEN 10375289 AND 10414160 THEN "995645982187227020679637"
-            WHEN block_number BETWEEN 10414160 AND 10465862 THEN "992331411409899697714436"
-            WHEN block_number BETWEEN 10465862 AND 10907589 THEN "990119942045004674110412"
-            WHEN block_number > 10907589 THEN "989437793194672047865689"
+            WHEN block_number < 10027387 THEN "9993210636.44775776713679" 
+            WHEN block_number BETWEEN 10027387 AND 10040222 THEN "9989825252.45810425296122"
+            WHEN block_number BETWEEN 10040222 AND 10065995 THEN "9987079469.32873941046141"
+            WHEN block_number BETWEEN 10065995 AND 10117398 THEN "9982194529.43352911059711"
+            WHEN block_number BETWEEN 10117398 AND 10169024 THEN "9974148447.14885566932754"
+            WHEN block_number BETWEEN 10169024 AND 10362451 THEN "9973170713.40081993793812"
+            WHEN block_number BETWEEN 10362451 AND 10375289 THEN "9970754597.72288395141677"
+            WHEN block_number BETWEEN 10375289 AND 10414160 THEN "9956459821.87227020679637"
+            WHEN block_number BETWEEN 10414160 AND 10465862 THEN "9923314114.09899697714436"
+            WHEN block_number BETWEEN 10465862 AND 10907589 THEN "9901199420.45004674110412"
+            WHEN block_number > 10907589 THEN "9894377931.94672047865689"
         END AS mantissa
       FROM
       `pod_transfers` 
@@ -138,11 +138,11 @@ CREATE TEMP TABLE pod_cutoff AS(
 CREATE TABLE v2_usdc_pods AS(
   SELECT 
     address,
-    value * 1e12 as value, -- scaling to be equal to dai/sai
+    value * 1e16 as value, -- scaling to be equal to dai/sai
     block_number,
     log_index,
     balance,
-    prev_balance * 1e18 as prev_balance,
+    prev_balance * 1e16 as prev_balance,
     delta_blocks
    FROM(
       SELECT * 
