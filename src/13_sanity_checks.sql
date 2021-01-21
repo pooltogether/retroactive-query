a9a121cb209f5b82dc59eafdfc3df8a22aa8c495
Aodhgan <aodhgan@pooltogether.com>
2021-01-21 13:12:23 -0800

    no of users

--- src/13_sanity_checks.sql
+++ src/13_sanity_checks.sql
@@ -25,59 +25,25 @@ BEGIN
                       FROM all_earnings) = 150000000 THEN TRUE
                 ELSE FALSE
                END)                                 AS passes
-    UNION ALL
---     SELECT "there are exactly 85 socks redeemers" AS test_case,
---         CAST((SELECT COUNT(1)
---                       FROM socks_redeemers) AS STRING) as test_value,
---            (CASE
---                 WHEN (SELECT COUNT(1)
---                       FROM socks_redeemers) = 85 THEN TRUE
---                 ELSE FALSE
---                END)                                 AS passes
---     UNION ALL
---     SELECT "there are exactly 151 socks holders" AS test_case,
---         CAST((SELECT COUNT(1)
---                       FROM socks_token_holders) AS STRING) as test_value,
---            (CASE
---                 WHEN (SELECT COUNT(1)
---                       FROM socks_token_holders) = 151 THEN TRUE
---                 ELSE FALSE
---                END)                                 AS passes
---     UNION ALL
---     SELECT "there are exactly 220 distinct socks holders + redeemers" AS test_case,
---         CAST((SELECT COUNT(1)
---                       FROM all_socks_users) AS STRING) as test_value,
---            (CASE
---                 WHEN (SELECT COUNT(1)
---                       FROM all_socks_users) = 220 THEN TRUE
---                 ELSE FALSE
---                END)                                 AS passes
---     UNION ALL
-    SELECT "there are exactly 251534 users" AS test_case,
-        CAST((SELECT COUNT(distinct address) from user_query) AS STRING) as test_value,
+    UNION AL
+    SELECT "there are exactly 16816 unique addresses" AS test_case,
+        CAST((SELECT COUNT(distinct address) from all_earnings) AS STRING) as test_value,
            (CASE
-                WHEN ((SELECT COUNT(distinct address) from user_query)) = 251534 THEN TRUE
+                WHEN ((SELECT COUNT(distinct address) from user_query)) = 16816 THEN TRUE
                 ELSE FALSE
                END)                                 AS passes
     UNION ALL
-    SELECT "no one gets less than the user_reward in UNI" AS test_case,
+    SELECT "no one gets less than the user_reward in POOL" AS test_case,
             CAST((SELECT MIN(earnings) FROM all_earnings) AS STRING) as test_value,
             (SELECT (MIN(earnings) = @user_reward) FROM all_earnings)
                                                     AS passes
     UNION ALL
-    SELECT "there are exactly 49191 liquidity providers" AS test_case,
-        CAST((SELECT COUNT(distinct address) from liquidity_provider_query) AS STRING) as test_value,
-           (CASE
-                WHEN ((SELECT COUNT(distinct address) from liquidity_provider_query)) = 49191 THEN TRUE
-                ELSE FALSE
-               END)                                 AS passes
-    UNION ALL
-    SELECT "there are no uniswap contracts" AS test_case,
+    SELECT "there are no naughty list contracts in the rewards" AS test_case,
         CAST((SELECT COUNT(*) from all_earnings
-              WHERE address NOT IN (SELECT DISTINCT address FROM uniswap_contracts)) AS STRING) as test_value,
+              WHERE address NOT IN (SELECT DISTINCT address FROM naughty_list)) AS STRING) as test_value,
            (CASE
                 WHEN (SELECT COUNT(*) from all_earnings
-              WHERE address NOT IN (SELECT DISTINCT address FROM uniswap_contracts)) = 0 THEN TRUE
+              WHERE address NOT IN (SELECT DISTINCT address FROM naughty_list)) = 0 THEN TRUE
                 ELSE FALSE
                END)                                 AS passes
                                                     );
